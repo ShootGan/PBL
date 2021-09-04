eeacd73a2b8619bb99eb208a0c68e7168578bd8b
Ponicefa <malump999@gmail.com>
2021-09-04 19:27:13 +0200

    Css fix

--- src/components/Card/CardWrapper.tsx
+++ src/components/Card/CardWrapper.tsx
@@ -1,12 +1,16 @@
 import styled from "@emotion/styled";
 
-const NavbarWrapper = styled.div`
+const CardWrapper = styled.div`
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   transition: 0.3s;
-  width: 95%;
-  padding: 2px 16px;
-  justify-content: center;
-  align-items: center;
-  border-radius: 8px;
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  border-radius: 10px;
+  background-color: lightgray;
+  margin-left: 5%;
+  margin-bottom: 5%;
 `;
-export default NavbarWrapper;
+
+export default CardWrapper;
--- src/components/Card/CaredImage.tsx
+++ src/components/Card/CaredImage.tsx
@@ -5,5 +5,6 @@ const CardImage = styled.img`
   border-radius: 10px 10px 0px 0px;
   justify-content: center;
   align-items: center;
+  max-height: 200px;
 `;
 export default CardImage;
